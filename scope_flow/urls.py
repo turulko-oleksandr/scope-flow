e5962a3cd48a33d5e622d5099474da4242eb81dd
turulko-oleksandr <SashaTurulko@i.ua>
2025-08-06 14:47:28 +0300

    feat(routing): configure URLs and test with base.html template

--- scope_flow/urls.py
+++ scope_flow/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from scope_flow.views import home_page
 
 urlpatterns = [
-    path('scope-flow/', include('scope_flow.urls')),
-    path('admin/', admin.site.urls),
+    path("", home_page, name="home-page"),
 ]
+
