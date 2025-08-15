ff71e25f3de0b30b9e1596c8bf08ba9d62d0b73f
turulko-oleksandr <SashaTurulko@i.ua>
2025-08-15 16:48:14 +0300

    feat(auth): add login page, fix navigation, and update styles

--- scope_flow_service/urls.py
+++ scope_flow_service/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', include('scope_flow.urls')),
+    path('scope-flow/', include('scope_flow.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/scope-flow/', permanent=True))
 ]
