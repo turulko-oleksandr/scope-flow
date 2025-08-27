f595208f28e4bc9b776205af75706111e3e82395
turulko-oleksandr <sashaturulko@i.ua>
2025-08-27 23:57:27 +0300

    fix(urls, env): fix urlpatterns for scope-flow namespace and moved secret key into .env

--- scope_flow_service/settings.py
+++ scope_flow_service/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
+import environ
 
 from pathlib import Path
 
@@ -16,11 +18,10 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_jg-e)s)$)c#)%cd@(oizy^xigi)2-etslk34hul)j^j=kus*6'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
